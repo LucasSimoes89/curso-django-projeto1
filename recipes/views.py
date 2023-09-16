73299dbe7dd84cfaf0bc4c80655d7e5267573e89
Lucas Simoes <lucas.ssimoes89@gmail.com>
2023-09-16 16:45:13 -0300

    Filtro contains e icontains com Q

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-
+from django.db.models import Q
 from .models import Recipe
 
 # from utils.recipes.factory import make_recipe
@@ -46,7 +46,14 @@ def search(request):
 
     if not search_term:
         raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term} |"',
         'search_term': search_term,
+        'recipes': recipes
     })
