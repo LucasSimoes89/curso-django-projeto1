9f41ec03164cd63733786621e7a3ad0e68667f9b
Lucas Simoes <lucas.ssimoes89@gmail.com>
2023-09-17 10:10:22 -0300

    criando função e teste paginação

---
+++ utils/test_pagination.py
@@ -0,0 +1,100 @@
+from unittest import TestCase
+
+from utils.pagination import make_pagination_range
+
+
+class PaginationTest(TestCase):
+    def test_make_pagination_range_returns_a_pagination_rage(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )['pagination']
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
+        # Currente Page = 1 | qty_pages = 2 | Midlge_page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )['pagination']
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # Currente Page = 2 | qty_pages = 2 | Midlge_page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )['pagination']
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # Currente Page = 3 | qty_pages = 2 | Midlge_page = 2
+        # Range should change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )['pagination']
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+        # Currente Page = 4 | qty_pages = 2 | Midlge_page = 2
+        # Range should change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=4,
+        )['pagination']
+        self.assertEqual([3, 4, 5, 6], pagination)
+
+    def test_make_sure_middle_range_are_correct(self):
+        # Currente Page = 10 | qty_pages = 2 | Midlge_page = 2
+        # Range should change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=10,
+        )['pagination']
+        self.assertEqual([9, 10, 11, 12], pagination)
+
+        # Currente Page = 12 | qty_pages = 2 | Midlge_page = 2
+        # Range should change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=12,
+        )['pagination']
+        self.assertEqual([11, 12, 13, 14], pagination)
+
+    def test_make_pagination_is_static_when_last_page_is_next(self):
+        # Currente Page = 18 | qty_pages = 2 | Midlge_page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=18,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # Currente Page = 19 | qty_pages = 2 | Midlge_page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=19,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # Currente Page = 20 | qty_pages = 2 | Midlge_page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=20,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # Currente Page = 21 | qty_pages = 2 | Midlge_page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=21,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
